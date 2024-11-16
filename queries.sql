1cff1711e7e2ea822b92fc0e8a9cd45f3befc67b
Amos Adejesubisi <amosstudyroom@gmail.com>
2024-11-16 11:50:48 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,141 +1,14 @@
---Overview of Weather, Demand, Wind Speed, and Humidity in 2017
---Average Demand by Temperature Category
-CREATE TABLE AvgDTempC( 
-Temp_Category	TEXT,
-avg_demand	REAL
-);
-INSERT INTO AvgDTempC
-SELECT tem.temp_category, AVG(car.demand) AS avg_demand
-FROM Temperaturetab tem
-JOIN CarSharing_df car ON tem.temp_code = car.temp_code
-JOIN Timetab tim on car.id = tim.id
+-- - **(a)**: Date and time with highest demand in 2017.
+--Finding the Date and Time with the Highest Demand Rate in 2017
+SELECT tim.timestamp as DatenTime, car.demand as DemandR
+FROM CarSharing_df car
+JOIN Timetab tim ON car.id = tim.id
 WHERE strftime('%Y', tim.timestamp) = '2017'
-GROUP BY temp_category
-ORDER BY avg_demand DESC;
-
-SELECT * FROM AvgDTempC
-
--- weather frequency
-CREATE TABLE MostWeath (
-Weather	TEXT,
-Frequency
-);
-INSERT INTO MostWeath
-SELECT weather, COUNT(*) AS Frequency
-FROM weathertab wea
-JOIN CarSharing_df car ON wea.weather_code = car.weather_code
-JOIN Timetab tim on car.id = tim.id
-WHERE strftime('%Y', tim.timestamp) = '2017'
-GROUP BY weather
-ORDER BY frequency DESC
+ORDER BY DemandR DESC
 LIMIT 1;
 
-SELECT * FROM MostWeath
-
--- Monthly Wind Speed and Humidity Statistics
-CREATE TABLE Monwinds (
-month	INT,
-avg_windspeed REAL,
-max_windspeed REAL,
-min_windspeed REAL
-);
-
-INSERT INTO Monwinds
-SELECT strftime('%m', tim.timestamp) AS month,
-       AVG(car.windspeed) AS avg_windspeed,
-       MAX(car.windspeed) AS max_windspeed,
-       MIN(car.windspeed) AS min_windspeed
-FROM Timetab tim
-JOIN CarSharing_df car on tim.id = car.id
-WHERE strftime('%Y', timestamp) = '2017'
-GROUP BY month
-ORDER BY month;
-
-SELECT * FROM Monwinds
-
-CREATE TABLE Monhums (
-month	INT,
-avg_humidity REAL,
-max_humidity REAL,
-min_humidity REAL
-);
-
-INSERT INTO Monhums
-SELECT strftime('%m', tim.timestamp) AS month,
-       AVG(car.humidity) AS avg_humidity,
-       MAX(car.humidity) AS max_humidity,
-       MIN(car.humidity) AS min_humidity
-FROM Timetab tim
-JOIN CarSharing_df car on tim.id = car.id
-WHERE strftime('%Y', timestamp) = '2017'
-GROUP BY month
-ORDER BY month;
-
-SELECT * FROM Monhums
-
-
--- Month with Highest Average Demand Wind and Hum
-CREATE TABLE HadMonwinhum (
-month_name TEXT,
-avg_windspeed REAL,
-max_windspeed REAL,
-min_windspeed REAL,
-avg_humidity REAL,
-max_humidity REAL,
-min_humidity REAL
-);
-INSERT INTO HadMonwinhum
-SELECT month_name,
-	   AVG(car.windspeed) AS avg_windspeed,
-       MAX(car.windspeed) AS max_windspeed,
-       MIN(car.windspeed) AS min_windspeed,
-	   AVG(car.windspeed) AS avg_humidity,
-       MAX(car.windspeed) AS max_humidity,
-       MIN(car.windspeed) AS min_humidity
-FROM CarSharing_df car
-JOIN timetab tim ON car.id = tim.id
-WHERE strftime('%Y', tim.timestamp) = '2017' AND strftime('%m', tim.timestamp) = '07';
-
-SELECT * FROM HadMonwinhum
-
--- Q.344 Hourly Average Demand for the Highest/Lowest Demand Weekday
-DROP table if EXISTS lowestday_HAD;
-drop table if EXISTS highestday_HAD;
-
---Hourly average demand for the Highest Demand Weekday
-CREATE TABLE Hday_HAD (
-	Hour TEXT,
-	avg_demand REAL
-);
-
-INSERT INTO Hday_HAD
-SELECT tim.hour, AVG(car.demand) AS avg_demand
-FROM CarSharing_df car
-JOIN Timetab tim on car.id = tim.id
-WHERE strftime('%Y', timestamp) = '2017' AND weekday_name = 'Sunday'
-GROUP BY Hour
-ORDER BY avg_demand DESC;
-
---Hourly average demand for the Lowest Demand Weekday
-CREATE TABLE Lday_HAD (
-	Hour TEXT,
-	avg_demand REAL
-);
-
-INSERT INTO Lday_HAD
-SELECT tim.hour as Hour, AVG(car.demand) AS avg_demand
-FROM CarSharing_df car
-JOIN Timetab tim on car.id = tim.id
-WHERE strftime('%Y', timestamp) = '2017' AND weekday_name = 'Thursday'
-GROUP BY hour
-ORDER BY avg_demand DESC;
-
-SELECT * FROM Hday_HAD
-SELECT * FROM lday_HAD
-
---Q. Creating Table for Weekday, Season and Month
-DROP TABLE IF EXISTS Weekday_Demand;
-
+-- - **(b)**: Weekday, month, and season with highest/lowest average demand in 2017.
+-- Creating Table for Weekday, Season and Month
 CREATE TABLE Weekday_Demand (
     weekday_name TEXT,
     avg_demand REAL
@@ -206,11 +79,169 @@ ORDER BY m_avg_demand ASC
 LIMIT 1;
 
 
---Date and Time with the Highest Demand Rate in 2017
---Finding the Date and Time with the Highest Demand Rate in 2017
-SELECT tim.timestamp as DatenTime, car.demand as DemandR
+-- - **(c)**: Hourly demand analysis for selected weekday in (b).
+--Hourly average demand for the Highest Demand Weekday
+CREATE TABLE Hday_HAD (
+	Hour TEXT,
+	avg_demand REAL
+);
+
+INSERT INTO Hday_HAD
+SELECT tim.hour, AVG(car.demand) AS avg_demand
 FROM CarSharing_df car
-JOIN Timetab tim ON car.id = tim.id
+JOIN Timetab tim on car.id = tim.id
+WHERE strftime('%Y', timestamp) = '2017' AND weekday_name = 'Sunday'
+GROUP BY Hour
+ORDER BY avg_demand DESC;
+
+--Hourly average demand for the Lowest Demand Weekday
+CREATE TABLE Lday_HAD (
+	Hour TEXT,
+	avg_demand REAL
+);
+
+INSERT INTO Lday_HAD
+SELECT tim.hour as Hour, AVG(car.demand) AS avg_demand
+FROM CarSharing_df car
+JOIN Timetab tim on car.id = tim.id
+WHERE strftime('%Y', timestamp) = '2017' AND weekday_name = 'Thursday'
+GROUP BY hour
+ORDER BY avg_demand DESC;
+
+SELECT * FROM Hday_HAD
+SELECT * FROM lday_HAD
+
+-- - **(d)**: Weather analysis for 2017.
+--Average Demand by Temperature Category
+CREATE TABLE AvgDTempC( 
+Temp_Category	TEXT,
+avg_demand	REAL
+);
+INSERT INTO AvgDTempC
+SELECT tem.temp_category, AVG(car.demand) AS avg_demand
+FROM Temperaturetab tem
+JOIN CarSharing_df car ON tem.temp_code = car.temp_code
+JOIN Timetab tim on car.id = tim.id
 WHERE strftime('%Y', tim.timestamp) = '2017'
-ORDER BY DemandR DESC
+GROUP BY temp_category
+ORDER BY avg_demand DESC;
+
+SELECT * FROM AvgDTempC
+
+-- weather frequency
+CREATE TABLE MostWeath (
+Weather	TEXT,
+Frequency
+);
+INSERT INTO MostWeath
+SELECT weather, COUNT(*) AS Frequency
+FROM weathertab wea
+JOIN CarSharing_df car ON wea.weather_code = car.weather_code
+JOIN Timetab tim on car.id = tim.id
+WHERE strftime('%Y', tim.timestamp) = '2017'
+GROUP BY weather
+ORDER BY frequency DESC
 LIMIT 1;
+
+SELECT * FROM MostWeath
+
+-- Monthly Wind Speed and Humidity Statistics 
+-- the monthly windspeed in 2017.
+CREATE TABLE Monwinds (
+month	INT,
+avg_windspeed REAL,
+max_windspeed REAL,
+min_windspeed REAL
+);
+
+INSERT INTO Monwinds
+SELECT strftime('%m', tim.timestamp) AS month,
+       AVG(car.windspeed) AS avg_windspeed,
+       MAX(car.windspeed) AS max_windspeed,
+       MIN(car.windspeed) AS min_windspeed
+FROM Timetab tim
+JOIN CarSharing_df car on tim.id = car.id
+WHERE strftime('%Y', timestamp) = '2017'
+GROUP BY month
+ORDER BY month;
+
+SELECT * FROM Monwinds
+
+-- the monthly humidity in 2017.
+CREATE TABLE Monhums (
+month	INT,
+avg_humidity REAL,
+max_humidity REAL,
+min_humidity REAL
+);
+
+INSERT INTO Monhums
+SELECT strftime('%m', tim.timestamp) AS month,
+       AVG(car.humidity) AS avg_humidity,
+       MAX(car.humidity) AS max_humidity,
+       MIN(car.humidity) AS min_humidity
+FROM Timetab tim
+JOIN CarSharing_df car on tim.id = car.id
+WHERE strftime('%Y', timestamp) = '2017'
+GROUP BY month
+ORDER BY month;
+
+SELECT * FROM Monhums
+
+
+-- - **(e)**: Weather analysis of month with highest demand.
+--Month with Highest Average Demand Temperature Category
+CREATE TABLE MonHadTempC( 
+Temp_Category	TEXT,
+avg_demand	REAL
+);
+INSERT INTO MonHadTempC
+SELECT tem.temp_category, AVG(car.demand) AS avg_demand
+FROM Temperaturetab tem
+JOIN CarSharing_df car ON tem.temp_code = car.temp_code
+JOIN Timetab tim on car.id = tim.id
+WHERE strftime('%Y', tim.timestamp) = '2017' AND strftime('%m', tim.timestamp) = '07'
+GROUP BY temp_category
+ORDER BY avg_demand DESC;
+
+SELECT * FROM MonHadTempC
+
+-- Month with Highest Average Demand weather frequency
+CREATE TABLE MonHadWeathF (
+Weather	TEXT,
+Frequency
+);
+INSERT INTO MonHadWeathF
+SELECT weather, COUNT(*) AS Frequency
+FROM weathertab wea
+JOIN CarSharing_df car ON wea.weather_code = car.weather_code
+JOIN Timetab tim on car.id = tim.id
+WHERE strftime('%Y', tim.timestamp) = '2017' AND strftime('%m', tim.timestamp) = '07'
+GROUP BY weather
+ORDER BY frequency DESC
+LIMIT 1;
+
+SELECT * FROM MonHadWeathF
+
+CREATE TABLE HadMonwinhum (
+month_name TEXT,
+avg_windspeed REAL,
+max_windspeed REAL,
+min_windspeed REAL,
+avg_humidity REAL,
+max_humidity REAL,
+min_humidity REAL
+);
+INSERT INTO HadMonwinhum
+SELECT month_name,
+	   AVG(car.windspeed) AS avg_windspeed,
+       MAX(car.windspeed) AS max_windspeed,
+       MIN(car.windspeed) AS min_windspeed,
+	   AVG(car.windspeed) AS avg_humidity,
+       MAX(car.windspeed) AS max_humidity,
+       MIN(car.windspeed) AS min_humidity
+FROM CarSharing_df car
+JOIN timetab tim ON car.id = tim.id
+WHERE strftime('%Y', tim.timestamp) = '2017' AND strftime('%m', tim.timestamp) = '07';
+
+SELECT * FROM HadMonwinhum
